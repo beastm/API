60b7a7068503099bbdb37f6b21a16435db71b6bb
Rustam <gasanovrustam61@gmail.com>
2020-04-11 16:45:50 +0200

    Add project files.

---
+++ WebAPI/Controllers/AdminController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class AdminController : ApiController
+    {
+        private MyContext context = new MyContext();
+
+
+        public IEnumerable<Admin_list> Get()
+        {
+            return this.context.Admins;
+        }
+
+     
+        public Admin_list Get(int id)
+        {
+            return this.context.Admins.Find(id);
+        }
+
+        // POST: api/Admins
+        public void Post([FromBody]Admin_list admin)
+        {
+            this.context.Admins.Add(admin);
+            this.context.SaveChanges();
+        }
+
+        // PUT: api/Admin/5
+        public void Put(int id, [FromBody]Admin_list admin)
+        {
+            Admin_list current = this.context.Admins.Find(id);
+
+            current.id = admin.id;
+            current.username = admin.username;
+            current.login = admin.login;
+            current.password = admin.password;
+            current.gmail = admin.gmail;
+
+
+
+            this.context.SaveChanges();
+        }
+
+        // DELETE: api/Admin/5
+        public void Delete(int id)
+        {
+            Admin_list person = this.context.Admins.Find(id);
+
+            this.context.Admins.Remove(person);
+            this.context.SaveChanges();
+        }
+    }
+}
---
+++ WebAPI/Models/Admin_list.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.Models
+{
+    public class Admin_list
+    {
+        public int id { get; set; }
+
+        public string username { get; set; }
+
+        public string login { get; set; }
+
+        public string password { get; set; }
+        public string gmail { get; set; }
+    }
+}
